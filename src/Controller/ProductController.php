e28379bc74f8256b28807e6adb96c9cfcbcda492
cellyasirot <cellya.sirot@gmail.com>
2021-12-13 12:01:57 +0100

    product filter feature

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use SearchType;
 use App\Classe\Search;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
-use SearchType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,11 +23,21 @@ class ProductController extends AbstractController
     /**
      * @Route("/nos-produits", name="products")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $search = new Search();
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
         $form = $this->createForm(SearchType::class, $search);
+
+        $form->handleRequest($request); // écoute si le formulaire est envoyé
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        }
+        else 
+        {
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
 
         return $this->render('product/index.html.twig', [
             'products' => $products, 
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -2,9 +2,10 @@
 
 namespace App\Repository;
 
+use App\Classe\Search;
 use App\Entity\Product;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @method Product|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,6 +18,24 @@ class ProductRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    public function findWithSearch(Search $search)
+    {
+        $query = $this 
+            ->createQueryBuilder('p')
+            ->select('c','p')
+            ->join('p.category', 'c');
+            // <=> Select * from product, category where product.category = category.id
+
+        if(!empty($search->categories))
+        {
+            $query=$query
+                ->andWhere('c.id IN (:categories)') // <=> and category.id in :value
+                ->setParameter('categories', $search->categories);
+        }
+
+        return $query->getQuery()->getResult();
     }
 
     // /**
